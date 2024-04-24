ad3bde7eff9a4fb61f19a8e6d8dc2baee6bcf967
DaynerKurdi <ziad_dayner@yahoo.com>
2024-04-24 19:34:34 +0300

    updateState

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -51,6 +51,29 @@ public class GameManager : MonoBehaviour
                     Debug.Log("My state is DataLoad");
                 }
                 break;
+                case GameStage.start:
+                {
+
+                    Debug.Log("My state is start");
+                }
+                break;
+                case GameStage.GameLoop:
+                {
+                    Debug.Log("My state is gameloop");
+                }
+                break;
+
+                case GameStage.GameEnd:
+                {
+                    Debug.Log("My state is gameend");
+                }
+                break;
+
+                case GameStage.destory: 
+                {
+                    Debug.Log("My state is destory");
+                }
+                break;
         }
     }
 }
